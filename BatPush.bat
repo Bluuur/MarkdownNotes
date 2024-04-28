9ad95e7096e70c2b13a765465ec97ad1595a6ed0
zhangzd <zidongzh@outlook.com>
2024-04-28 11:58:45 +0800

    2024/04/28 周日 11:58:44.91 update

--- BatPush.bat
+++ BatPush.bat
@@ -1,23 +1,17 @@
 @echo off
-echo "GIT PUSH BAT"
-
-echo "Moving to working directory" 
-D:
-cd D:\Projects\MarkdownNotes
-
-echo "Start submitting code to the local repository"
-git add *
- 
-echo "Commit the changes to the local repository"
+echo "GIT PUSH BAT running" > repoSync.log
 set now=%date% %time%
-echo "Time:" %now%
-git commit -m "%now% update"
- 
-echo "Push the changes to the remote git server"
-git push origin main
- 
-echo "Execution complete!"
+echo "Time:" %now% >> repoSync.log
 
-pause
+echo "Start submitting code to the local repository" >> repoSync.log
+git add . >> repoSync.log
+ 
+echo "Commit the changes to the local repository" >> repoSync.log
+git commit -m "%now% update" >> repoSync.log
+ 
+echo "Push the changes to the remote git server" >> repoSync.log
+git push origin main >> repoSync.log
+ 
+echo "Execution complete!" >> repoSync.log
 
 exit
